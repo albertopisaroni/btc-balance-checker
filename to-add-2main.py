14eb0e06f6422b995b5790c12bd857c5ca052989
Alberto Pisaroni <45979126+albertopisaroni@users.noreply.github.com>
2022-02-03 02:49:58 +0100

    Create to-add-2main.py

---
+++ to-add-2main.py
@@ -0,0 +1,69 @@
+
+import sys
+import re
+from time import sleep
+from urllib.request import urlopen
+
+
+
+def check_balance(address):
+
+    #Modify the value of the variable below to False if you do not want Bell Sound when the Software finds balance.
+    SONG_BELL = True
+
+    #Add time different of 0 if you need more security on the checks
+    WARN_WAIT_TIME = 0
+
+    blockchain_tags_json = [ 
+        'total_received',
+        'final_balance',
+        ]
+
+    SATOSHIS_PER_BTC = 1e+8
+
+    check_address = address
+
+    parse_address_structure = re.match(r' *([a-zA-Z1-9]{1,34})', check_address)
+    if ( parse_address_structure is not None ):
+        check_address = parse_address_structure.group(1)
+    else:
+        print( "\nThis Bitcoin Address is invalid" + check_address )
+        exit(1)
+
+    #Read info from Blockchain about the Address
+    reading_state=1
+    while (reading_state):
+        try:
+            htmlfile = urlopen("https://blockchain.info/address/%s?format=json" % check_address, timeout = 10)
+            htmltext = htmlfile.read().decode('utf-8')
+            reading_state  = 0
+        except:
+            reading_state+=1
+            print( "Checking... " + str(reading_state) )
+            sleep(60*reading_state)
+
+    print( "\nBitcoin Address = " + check_address )
+
+    blockchain_info_array = []
+    tag = ''
+    try:
+        for tag in blockchain_tags_json:
+            blockchain_info_array.append (
+                float( re.search( r'%s":(\d+),' % tag, htmltext ).group(1) ) )
+    except:
+        print( "Error '%s'." % tag );
+        exit(1)
+
+    for i, btc_tokens in enumerate(blockchain_info_array):
+
+        sys.stdout.write ("%s \t= " % blockchain_tags_json[i])
+        if btc_tokens > 0.0:
+            print( "%.8f Bitcoin" % (btc_tokens/SATOSHIS_PER_BTC) );
+        else:
+            print( "0 Bitcoin" );
+
+
+#Add the filename of your list of Bitcoin Addresses for check all.
+
+check_balance("199CZeJwriVa7oEh1sz92nsiMAzmCW3Nz2")
+
