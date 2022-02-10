5aaea8df9f2b0719dc81039df20029ea82edb503
Alberto Pisaroni <45979126+albertopisaroni@users.noreply.github.com>
2022-02-10 17:33:59 +0100

    Update checker.py

--- checker.py
+++ checker.py
@@ -1,13 +1,43 @@
-from moneywagon import AddressBalance
+import sys
+import re
 
-def balance(addr):
-	total = AddressBalance().action('btc', addr)
-	balanceout= str(total)
+from moneywagon import AddressBalance
+from time import sleep
+from urllib.request import urlopen
+
+def alladdr(addr):
+    global balanceout
+    total = AddressBalance().action('btc', addr)
+    balanceout= str(total)
+
+def nobc1addr(addr):
+    global balanceout
+    satoshis = 1e+8
+    
+    htmlfile = urlopen("https://blockchain.info/address/%s?format=json" % addr, timeout = 10)
+    htmltext = htmlfile.read().decode('utf-8')
+    btc_tokens = float( re.search( r'%s":(\d+),' % 'final_balance', htmltext ).group(1) )
+    balanceout = str(btc_tokens/satoshis)
 
 
 def main():
-	addr = input("Enter an Address :>  ")
-	balance(addr)
+
+    print("\n-----------------------------------------------------------")
+    print("            albertopisaroni/btc-balance-checker")
+    print("-----------------------------------------------------------\n")
+
+    addr = input("Enter a BTC Address :>  ")
+
+    try:
+        try:
+            nobc1addr(addr)
+        except:
+            alladdr(addr)
+    except:
+        print("Wallet address is wrong.")
+        exit(1)
+
+    print("     You have found :>  " + balanceout + " BTC")
 
 if __name__ == '__main__':
-	main()
+    main()
